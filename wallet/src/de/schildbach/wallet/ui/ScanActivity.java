a93ba72f49b6d1266e6a3acfa92188ed3736a75c
Andreas Schildbach <andreas@schildbach.de>
2012-03-03 13:46:01 +0100

    prod: Remove 'test' flavor from version.

--- wallet/src/de/schildbach/wallet/ui/ScanActivity.java
+++ wallet/src/de/schildbach/wallet/ui/ScanActivity.java
@@ -62,7 +62,7 @@ import com.google.zxing.common.HybridBinarizer;
 import com.google.zxing.qrcode.QRCodeReader;
 
 import de.schildbach.wallet.camera.CameraManager;
-import de.schildbach.wallet_test.R;
+import de.schildbach.wallet.R;
 
 /**
  * @author Andreas Schildbach
